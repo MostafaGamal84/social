2c624d29aa55e883ce07b4668039e86a38ca77a1
Mostafa Gamal <mostafa.ddamoni22@gmail.com>
2025-10-28 14:57:23 +0300

    sad

--- API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272cd906a981fcdd0059d01f5e6f3dc2fcd47678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b543f128ef5cee46a2a649a253d6bdc5bbac1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
