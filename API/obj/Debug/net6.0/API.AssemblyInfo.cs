b2b4dcb334b2ed71cffe54845c8432bab5379268
Mostafa Gamal <mostafa.ddamoni22@gmail.com>
2025-10-28 16:40:51 +0300

    dhdf

--- API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cb32e0b3249f4e23d8e0f2ea6dd2cfa9a16d01d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e92ff93b1b235c8328edb68c96b8137d9ebb60b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
