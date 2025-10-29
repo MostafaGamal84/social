6d633b330ea3a324fe495ace7f4b2f4d3e334bfd
Mostafa Gamal <mostafa.ddamoni22@gmail.com>
2025-10-29 15:15:33 +0300

    gfdg

--- API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a6c5c8b83568e941f15cb36abe3ebe1d314411d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6947d65984730c1d8b796bfe6641805bd44217f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
